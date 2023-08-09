36a1fa5fc90449b0070d2580d3c00d0aa24f0e69
Paulina Dąbrowska <paulina.dabrovska@gmail.com>
2023-08-09 23:26:14 +0200

    add targaryen-(lannister)-cards.jss, got-gwent.jss

---
+++ scripts/lannister-cards.js
@@ -0,0 +1,26 @@
+export const houseLannisterCards =[
+  {
+    name: 'Jaime Lannister',
+    description: 'ser Jaime',
+    img: "jaime-lannister.jpg",
+    value: 1,
+    ability: "view.png",
+    type: "sword.png"
+  },
+  {
+    name: 'Tyrion Lannister',
+    description: 'Tyrion',
+    img: "jaime-lannister.jpg",
+    value: 9,
+    ability: "view.png",
+    type: "bow.png"
+  },
+  {
+    name: 'Tywin Lannister',
+    description: 'Casterly Rock',
+    img: "jaime-lannister.jpg",
+    value: 10,
+    ability: "view.png",
+    type: "catapult.png"
+  },
+];
---
+++ scripts/targaryen-cards.js
@@ -0,0 +1,26 @@
+export const houseTargaryenCards =[
+  {
+    name: 'Daenerys Targaryen',
+    description: 'Mother of Dragons',
+    img: "jaime-lannister.jpg",
+    value: 1,
+    ability: "view.png",
+    type: "bow.png"
+  },
+  {
+    name: 'Daemon Targaryen',
+    description: 'Prince Daemon Targaryen',
+    img: "jaime-lannister.jpg",
+    value: 9,
+    ability: "view.png",
+    type: "sword.png"
+  },
+  {
+    name: 'Rhaenyra Targaryen',
+    description: 'Queen Rhaenyra Targaryen is the daughter',
+    img: "jaime-lannister.jpg",
+    value: 10,
+    ability: "view.png",
+    type: "catapult.png"
+  },
+];
